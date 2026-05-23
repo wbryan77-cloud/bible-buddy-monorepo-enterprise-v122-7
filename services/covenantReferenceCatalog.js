a83f67d88f7b4568667c113c43ae03ce0e2e9995
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:33:17 -0700

    Add covenant reference catalog

---
+++ services/covenantReferenceCatalog.js
@@ -0,0 +1,14 @@
+const COVENANT_REFERENCE_CATALOG = {
+  covenantChains: [
+    'Genesis 9:8-17',
+    'Genesis 12:1-3',
+    'Genesis 15:1-21',
+    'Exodus 19:1-8',
+    'Deuteronomy 28:1-68',
+    'Jeremiah 31:31-34',
+    'Luke 22:14-20',
+    'Hebrews 8:1-13'
+  ]
+};
+
+module.exports = { COVENANT_REFERENCE_CATALOG };
