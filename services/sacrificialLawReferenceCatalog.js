aff8fc729c0bdd0e3cc946cb640fcbe0b96a71e6
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 12:24:50 -0700

    Add sacrificial law reference catalog

---
+++ services/sacrificialLawReferenceCatalog.js
@@ -0,0 +1,41 @@
+const SACRIFICIAL_LAW_REFERENCE_CATALOG = {
+  sacrificeAndPriesthoodChains: [
+    'Leviticus 1:1-17',
+    'Leviticus 4:1-35',
+    'Leviticus 16:1-34',
+    'Numbers 28:1-31',
+    'Numbers 29:1-40',
+    'Daniel 9:26-27',
+    'Matthew 27:50-51',
+    'Mark 15:37-38',
+    'Luke 23:44-46',
+    'Hebrews 7:11-28',
+    'Hebrews 8:1-13',
+    'Hebrews 9:1-28',
+    'Hebrews 10:1-18'
+  ],
+  obedienceOverSacrificeChains: [
+    '1 Samuel 15:22-23',
+    'Psalm 40:6-8',
+    'Psalm 51:16-17',
+    'Proverbs 21:3',
+    'Isaiah 1:11-20',
+    'Hosea 6:6',
+    'Micah 6:6-8',
+    'Matthew 9:13',
+    'Matthew 12:7'
+  ],
+  lawContinuityChains: [
+    'Matthew 5:17-19',
+    'Romans 3:31',
+    'Romans 7:7-12',
+    '1 Corinthians 7:19',
+    '1 John 2:3-6',
+    '1 John 5:2-3',
+    'Revelation 12:17',
+    'Revelation 14:12',
+    'Revelation 22:14'
+  ]
+};
+
+module.exports = { SACRIFICIAL_LAW_REFERENCE_CATALOG };
