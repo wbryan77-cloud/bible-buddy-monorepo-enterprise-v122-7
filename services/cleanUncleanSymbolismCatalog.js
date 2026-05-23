3ada056de5d710a69857bf6d3ae737a50ace8078
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:46:22 -0700

    Add clean unclean symbolism catalog

---
+++ services/cleanUncleanSymbolismCatalog.js
@@ -0,0 +1,12 @@
+const CLEAN_UNCLEAN_SYMBOLISM_CATALOG = {
+  symbolismChains: [
+    'Leviticus 11:1-47',
+    'Deuteronomy 14:1-21',
+    'Isaiah 52:11',
+    'Ezekiel 44:23',
+    'Acts 10:1-48',
+    '2 Corinthians 6:14-18'
+  ]
+};
+
+module.exports = { CLEAN_UNCLEAN_SYMBOLISM_CATALOG };
