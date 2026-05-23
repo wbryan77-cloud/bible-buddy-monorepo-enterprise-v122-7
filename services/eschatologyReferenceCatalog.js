86d28ad554c37dc568eb9fb26dd110ceac285f07
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:05:00 -0700

    Add eschatology reference catalog

---
+++ services/eschatologyReferenceCatalog.js
@@ -0,0 +1,24 @@
+const ESCHATOLOGY_REFERENCE_CATALOG = {
+  mountOlivesChain: [
+    'Zechariah 14:1-9',
+    'Matthew 24:27-31',
+    'Mark 13:24-27',
+    'Luke 21:25-28',
+    'Acts 1:9-12',
+    '1 Thessalonians 4:13-18',
+    'Revelation 1:7',
+    'Revelation 11:15',
+    'Revelation 19:11-21'
+  ],
+  pentecostStudyChain: [
+    'Leviticus 23:15-22',
+    'Deuteronomy 16:9-12',
+    'Acts 2:1-4',
+    'Acts 20:16',
+    '1 Corinthians 16:8',
+    '1 Thessalonians 4:16-17',
+    'Revelation 11:15'
+  ]
+};
+
+module.exports = { ESCHATOLOGY_REFERENCE_CATALOG };
