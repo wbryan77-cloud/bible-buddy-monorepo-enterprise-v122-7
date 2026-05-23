6eff52ef6f059ca6e4e2e4f3fbcdac8b2ebfe384
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:10:24 -0700

    Add resurrection reference catalog

---
+++ services/resurrectionReferenceCatalog.js
@@ -0,0 +1,10 @@
+const RESURRECTION_REFERENCE_CATALOG = {
+  resurrectionChains: [
+    'Daniel 12:1-3',
+    'John 5:24-29',
+    '1 Corinthians 15:1-58',
+    '1 Thessalonians 4:13-18'
+  ]
+};
+
+module.exports = { RESURRECTION_REFERENCE_CATALOG };
