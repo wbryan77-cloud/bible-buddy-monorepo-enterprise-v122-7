eca0160f0ec46510f2c8619e5b35f6e99067290f
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:46:39 -0700

    Add beast system reference catalog

---
+++ services/beastSystemReferenceCatalog.js
@@ -0,0 +1,12 @@
+const BEAST_SYSTEM_REFERENCE_CATALOG = {
+  beastChains: [
+    'Daniel 2:31-45',
+    'Daniel 7:1-28',
+    'Matthew 24:1-51',
+    '2 Thessalonians 2:1-12',
+    'Revelation 13:1-18',
+    'Revelation 17:1-18'
+  ]
+};
+
+module.exports = { BEAST_SYSTEM_REFERENCE_CATALOG };
