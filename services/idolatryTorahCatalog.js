cbb97178e0efd661944ebc3cfa8f3b39ef2e655c
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:42:06 -0700

    Add idolatry Torah catalog

---
+++ services/idolatryTorahCatalog.js
@@ -0,0 +1,10 @@
+const IDOLATRY_TORAH_CATALOG = {
+  idolatryChains: [
+    'Exodus 20:1-6',
+    'Deuteronomy 4:15-24',
+    'Jeremiah 3:1-14',
+    'Ezekiel 16:1-63'
+  ]
+};
+
+module.exports = { IDOLATRY_TORAH_CATALOG };
