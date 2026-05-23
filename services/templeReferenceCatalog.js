7779cc56dc77f8ae411588a00a7d7f9ae3acd737
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:33:31 -0700

    Add temple reference catalog

---
+++ services/templeReferenceCatalog.js
@@ -0,0 +1,13 @@
+const TEMPLE_REFERENCE_CATALOG = {
+  templeChains: [
+    'Exodus 25:1-40',
+    '1 Kings 6:1-38',
+    '2 Chronicles 7:1-22',
+    'Ezekiel 40:1-49',
+    'Matthew 24:1-15',
+    'John 2:13-22',
+    'Revelation 21:1-27'
+  ]
+};
+
+module.exports = { TEMPLE_REFERENCE_CATALOG };
