be1b31df710c789ee592a8060f98874e090d29b2
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:33:58 -0700

    Add wilderness reference catalog

---
+++ services/wildernessReferenceCatalog.js
@@ -0,0 +1,13 @@
+const WILDERNESS_REFERENCE_CATALOG = {
+  wildernessChains: [
+    'Exodus 16:1-36',
+    'Numbers 14:1-45',
+    'Deuteronomy 8:1-20',
+    'Psalm 78:1-72',
+    'Matthew 4:1-11',
+    '1 Corinthians 10:1-13',
+    'Hebrews 3:1-19'
+  ]
+};
+
+module.exports = { WILDERNESS_REFERENCE_CATALOG };
