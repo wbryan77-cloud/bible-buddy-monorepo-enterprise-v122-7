af73af62c5b2c5f74b719410555f0e4539612756
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:41:15 -0700

    Add faith and works catalog

---
+++ services/faithWorksCatalog.js
@@ -0,0 +1,12 @@
+const FAITH_WORKS_CATALOG = {
+  faithWorksChains: [
+    'Deuteronomy 10:12-13',
+    'Habakkuk 2:1-4',
+    'Matthew 5:17-20',
+    'Romans 3:21-31',
+    'James 2:14-26',
+    '1 John 2:1-6'
+  ]
+};
+
+module.exports = { FAITH_WORKS_CATALOG };
