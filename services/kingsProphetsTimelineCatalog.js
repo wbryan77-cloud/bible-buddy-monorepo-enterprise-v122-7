8bd64b90491d1dd7d5cb061b03756477b4a33b45
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:46:05 -0700

    Add kings and prophets timeline catalog

---
+++ services/kingsProphetsTimelineCatalog.js
@@ -0,0 +1,16 @@
+const KINGS_PROPHETS_TIMELINE_CATALOG = {
+  timelineChains: [
+    '1 Samuel 8:1-22',
+    '2 Samuel 7:1-29',
+    '1 Kings 11:1-43',
+    '1 Kings 18:1-46',
+    '2 Kings 17:1-41',
+    '2 Kings 25:1-30',
+    'Isaiah 1:1-31',
+    'Jeremiah 25:1-14',
+    'Ezekiel 1:1-28',
+    'Daniel 7:1-28'
+  ]
+};
+
+module.exports = { KINGS_PROPHETS_TIMELINE_CATALOG };
