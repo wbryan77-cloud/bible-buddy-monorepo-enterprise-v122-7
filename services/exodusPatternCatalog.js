a6eb535beb6d835820188ee2940228ac0f28155b
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:40:27 -0700

    Add Exodus pattern catalog

---
+++ services/exodusPatternCatalog.js
@@ -0,0 +1,13 @@
+const EXODUS_PATTERN_CATALOG = {
+  exodusChains: [
+    'Exodus 12:1-51',
+    'Exodus 13:1-22',
+    'Exodus 14:1-31',
+    'Exodus 19:1-25',
+    'Deuteronomy 8:1-20',
+    'Psalm 78:1-72',
+    '1 Corinthians 10:1-13'
+  ]
+};
+
+module.exports = { EXODUS_PATTERN_CATALOG };
