e8bcdd88813e9d35689b309b32180a9e157ad569
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:07:59 -0700

    Add fruits reference catalog

---
+++ services/fruitsReferenceCatalog.js
@@ -0,0 +1,12 @@
+const FRUITS_REFERENCE_CATALOG = {
+  fruitsChains: [
+    'Psalm 1:1-3',
+    'John 15:1-8',
+    'Galatians 5:22-23',
+    'Ephesians 5:8-10',
+    'Colossians 3:12-17',
+    '2 Peter 1:5-8'
+  ]
+};
+
+module.exports = { FRUITS_REFERENCE_CATALOG };
