d06884c9bf209340fa1ac8190908037a82ce09eb
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:41:33 -0700

    Add prayer and fasting catalog

---
+++ services/prayerFastingCatalog.js
@@ -0,0 +1,12 @@
+const PRAYER_FASTING_CATALOG = {
+  prayerFastingChains: [
+    'Isaiah 58:1-14',
+    'Daniel 9:1-23',
+    'Joel 2:12-17',
+    'Matthew 6:1-18',
+    'Matthew 17:14-21',
+    'Acts 13:1-3'
+  ]
+};
+
+module.exports = { PRAYER_FASTING_CATALOG };
