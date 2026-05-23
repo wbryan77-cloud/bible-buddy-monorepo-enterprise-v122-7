e165f91c36969624316e1eba0dea2652469f0c4b
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:09:45 -0700

    Add wisdom literature reference catalog

---
+++ services/wisdomReferenceCatalog.js
@@ -0,0 +1,13 @@
+const WISDOM_REFERENCE_CATALOG = {
+  wisdomChains: [
+    'Proverbs 1:1-7',
+    'Proverbs 2:1-22',
+    'Proverbs 3:1-35',
+    'Ecclesiastes 12:13-14',
+    'Job 28:12-28',
+    'Psalm 1:1-6',
+    'Psalm 119:97-105'
+  ]
+};
+
+module.exports = { WISDOM_REFERENCE_CATALOG };
