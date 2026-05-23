d196da8659646439b8b759182186f71adcdddc74
wbryan77-cloud <wbryan77@gmail.com>
2026-05-22 21:35:49 -0700

    Add scripture continuity enforcement rules

---
+++ services/doctrineContinuityRules.js
@@ -0,0 +1,48 @@
+const CONTINUITY_RULES = {
+  sabbath: {
+    foundation: [
+      'Genesis 2:2-3',
+      'Exodus 20:8-11',
+      'Isaiah 58:13-14',
+      'Luke 4:16',
+      'Acts 17:2',
+      'Hebrews 4:9'
+    ],
+    forbiddenPhrases: [
+      'Sunday replaced the Sabbath',
+      'the Sabbath was abolished',
+      'God changed the Sabbath to Sunday'
+    ]
+  },
+  dietaryLaw: {
+    foundation: [
+      'Leviticus 11',
+      'Deuteronomy 14',
+      'Daniel 1',
+      'Acts 10:14',
+      'Acts 10:28',
+      'Acts 11:1-18',
+      'Isaiah 66:17'
+    ],
+    forbiddenPhrases: [
+      'dietary laws were abolished',
+      'Acts 10 changed food laws',
+      'unclean foods are now clean'
+    ]
+  },
+  traditions: {
+    foundation: [
+      'Jeremiah 10:1-4',
+      'Mark 7:6-13',
+      'Colossians 2:8'
+    ],
+    forbiddenPhrases: [
+      'tradition overrides scripture',
+      'church tradition is equal to scripture'
+    ]
+  }
+};
+
+module.exports = {
+  CONTINUITY_RULES,
+};
