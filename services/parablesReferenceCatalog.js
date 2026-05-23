03dba05c55c7047cb0de5a1066ad98b7d2c190cc
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:10:02 -0700

    Add kingdom parables reference catalog

---
+++ services/parablesReferenceCatalog.js
@@ -0,0 +1,12 @@
+const PARABLES_REFERENCE_CATALOG = {
+  kingdomParableChains: [
+    'Matthew 13:1-52',
+    'Mark 4:1-34',
+    'Luke 8:4-18',
+    'Luke 15:1-32',
+    'Matthew 22:1-14',
+    'Matthew 25:1-46'
+  ]
+};
+
+module.exports = { PARABLES_REFERENCE_CATALOG };
