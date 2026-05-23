d668442dab3062585cd6317d608b37dd3a26a0fd
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:11:28 -0700

    Add priesthood Torah reference catalog

---
+++ services/priesthoodTorahCatalog.js
@@ -0,0 +1,11 @@
+const PRIESTHOOD_TORAH_CATALOG = {
+  priesthoodChains: [
+    'Genesis 14:18-20',
+    'Exodus 28:1-43',
+    'Leviticus 8:1-36',
+    'Numbers 18:1-32',
+    'Psalm 110:1-4'
+  ]
+};
+
+module.exports = { PRIESTHOOD_TORAH_CATALOG };
