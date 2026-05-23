23f4625af9585636e5403ddbf5834fd0ac246066
wbryan77-cloud <wbryan77@gmail.com>
2026-05-22 21:56:17 -0700

    Add continuity regression tests

---
+++ tests/continuityRegression.test.js
@@ -0,0 +1,18 @@
+const regressionCases = [
+  {
+    id: 'sabbath_continuity',
+    forbidden: ['Sunday replaced the Sabbath'],
+  },
+  {
+    id: 'dietary_continuity',
+    forbidden: ['dietary laws were abolished'],
+  },
+  {
+    id: 'tradition_override',
+    forbidden: ['church tradition overrides scripture'],
+  },
+];
+
+module.exports = {
+  regressionCases,
+};
