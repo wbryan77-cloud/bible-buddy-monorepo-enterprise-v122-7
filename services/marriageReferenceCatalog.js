86c2d93c899e8ff102b7827cd01dc41e32ae42c2
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:08:12 -0700

    Add marriage reference catalog

---
+++ services/marriageReferenceCatalog.js
@@ -0,0 +1,12 @@
+const MARRIAGE_REFERENCE_CATALOG = {
+  marriageChains: [
+    'Genesis 2:18-24',
+    'Malachi 2:13-16',
+    'Matthew 19:3-9',
+    'Mark 10:6-9',
+    'Ephesians 5:22-33',
+    'Revelation 19:7-9'
+  ]
+};
+
+module.exports = { MARRIAGE_REFERENCE_CATALOG };
