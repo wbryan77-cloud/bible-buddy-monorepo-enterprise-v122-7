d9ae6a6a18c5b923807bdcab3ae9315db5ed16e0
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:34:17 -0700

    Add Babylon reference catalog

---
+++ services/babylonReferenceCatalog.js
@@ -0,0 +1,14 @@
+const BABYLON_REFERENCE_CATALOG = {
+  babylonChains: [
+    'Genesis 11:1-9',
+    'Isaiah 13:1-22',
+    'Jeremiah 50:1-46',
+    'Jeremiah 51:1-64',
+    'Daniel 1:1-21',
+    'Daniel 2:1-49',
+    'Revelation 17:1-18',
+    'Revelation 18:1-24'
+  ]
+};
+
+module.exports = { BABYLON_REFERENCE_CATALOG };
