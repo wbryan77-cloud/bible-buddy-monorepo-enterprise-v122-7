0aa6bf5237789e5133126a1aa0be2235b04d851f
wbryan77-cloud <wbryan77@gmail.com>
2026-05-23 01:10:54 -0700

    Add prophecy symbol reference catalog

---
+++ services/prophecySymbolCatalog.js
@@ -0,0 +1,13 @@
+const PROPHECY_SYMBOL_CATALOG = {
+  prophecyChains: [
+    'Daniel 2:31-45',
+    'Daniel 7:1-28',
+    'Zechariah 4:1-14',
+    'Matthew 24:1-51',
+    'Revelation 1:1-20',
+    'Revelation 12:1-17',
+    'Revelation 17:1-18'
+  ]
+};
+
+module.exports = { PROPHECY_SYMBOL_CATALOG };
